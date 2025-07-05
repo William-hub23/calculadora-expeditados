be1c88691fe37f7795d613b6685161e8dc153802
William-hub23 <williamochoaitnl@gmail.com>
2025-07-05 12:06:23 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown(
 )
 
 # --- ENCABEZADO ---
-st.markdown("<h1 style='color:#FB6500;'>ALA Calculadora de Venta Viajes Expeditados</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color:#FB6500;'>🚛 Calculadora de Venta de Viajes Expeditados</h1>", unsafe_allow_html=True)
 
 # --- ACCESO CON CONTRASEÑA BÁSICA ---
 if 'authenticated' not in st.session_state:
